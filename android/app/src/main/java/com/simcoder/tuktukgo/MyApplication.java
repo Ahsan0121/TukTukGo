a459b38400d6c5a0698a9aa90afa89dfda59c915
Ahsan   Akbar <ahsanakbar025@gmial.com>
2024-07-27 08:17:26 -0700

    code added

---
+++ android/app/src/main/java/com/simcoder/tuktukgo/MyApplication.java
@@ -0,0 +1,22 @@
+package com.simcoder.tuktukgo;
+
+import android.app.Application;
+
+import com.onesignal.OneSignal;
+import com.simcoder.tuktukgo.Login.LauncherActivity;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Initialize any libraries here, e.g., OneSignal, Firebase, etc.
+//        call login launcher activity here
+//        print hello world
+
+
+//        val intent = Intent(this, LauncherActivity::class.java)
+//        startActivity(intent)
+
+    }
+
+}
